511c682f33ce66f2a7b23b8903fa97141e00e98f
mellivora24 <thanh139ptit@gmail.com>
2025-03-10 10:48:01 +0700

    Clean folder

---
+++ lib/utils/routers.dart
@@ -0,0 +1,9 @@
+import 'package:yourhome/views/screens/landing_page/LandingPage.dart';
+
+class AppRoutes {
+  static const LANDINGPAGE = '/landingpage';
+
+  static final routes = {
+    LANDINGPAGE: (context) => LandingPage(),
+  };
+}
---
+++ lib/views/screens/landing_page/LandingPage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Your Home',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        child: Text('Login'),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/register');
+                        },
+                        child: Text('Register'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+      )
+    );
+  }
+}
